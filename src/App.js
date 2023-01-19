a15f5598311f45dd3d1a8f828a5ef1a4cf8a75dc
Rio Edwards <rioredwards@gmail.com>
2023-01-19 15:06:14 -0800

    Setup empty main component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Layout/Header/Header.js';
 import Footer from './components/Layout/Footer/Footer.js';
+import Main from './components/Layout/Main/Main.js';
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <Header />
+      <Main />
       <Footer />
     </>
   );
---
+++ src/components/Layout/Main/Main.js
@@ -0,0 +1,9 @@
+import './Main.css';
+
+export default function Main() {
+  return (
+    <main>
+      <h2>This is main!</h2>
+    </main>
+  );
+}
