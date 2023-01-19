7a4821c42dfc90c8f2df2f688732175e2793226f
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-19 14:52:03 -0800

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Layout/Header/Header.js';
+import Footer from './components/Layout/Footer/Footer.js';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Footer />
     </>
   );
 }
