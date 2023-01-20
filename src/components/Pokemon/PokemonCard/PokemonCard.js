d18e1f19467712ac5bd03524a5c3da70a3c94270
Rio Edwards <rioredwards@gmail.com>
2023-01-20 14:08:03 -0800

    Add more pokemon attributes: image, color, and id

--- src/components/Pokemon/PokemonCard/PokemonCard.js
+++ src/components/Pokemon/PokemonCard/PokemonCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export default function PokemonCard({ name, type_1, type_2 }) {
+export default function PokemonCard({ id, name, type_1, type_2, image, color_1, color_2 }) {
   return (
-    <div>
-      {name} {type_1} {type_2}
+    <div style={{ backgroundColor: color_1 }}>
+      <div>
+        <h1>{name}</h1>
+        <p>{id}</p>
+        <p>{type_1}</p>
+        <p>{type_2}</p>
+      </div>
+      <img src={image} alt={name}></img>
     </div>
   );
 }
--- src/components/Pokemon/PokemonList.js
+++ src/components/Pokemon/PokemonList.js
@@ -5,7 +5,16 @@ export default function PokemonList({ pokemon, loading }) {
   return (
     <div>
       {pokemon.map((poke) => (
-        <PokemonCard key={poke._id} name={poke.pokemon} type_1={poke.type_1} type_2={poke.type_2} />
+        <PokemonCard
+          key={poke._id}
+          id={poke.id}
+          name={poke.pokemon}
+          type_1={poke.type_1}
+          type_2={poke.type_2}
+          color_1={poke.color_1}
+          color_2={poke.color_2}
+          image={poke.url_image}
+        />
       ))}
     </div>
   );
