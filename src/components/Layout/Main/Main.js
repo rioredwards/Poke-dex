958731326d880797f6f0697b2fee8117c366c78f
Rio Edwards <rioredwards@gmail.com>
2023-01-23 11:33:35 -0800

    Seperate query input value and submit value and remove 'handleChange'. UseEffect takes care of querySubmit

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -5,15 +5,14 @@ import PokemonList from '../../Pokemon/PokemonList.js';
 import './Main.css';
 
 export default function Main() {
-  const { pokemon, types, handleTypeChange, query, setQuery, handleFormSubmit, loading, error } =
-    usePokemon();
+  const { pokemon, types, handleTypeChange, setQuerySubmitValue, loading, error } = usePokemon();
 
   return (
     <main>
       <p>{error}</p>
       <div className="controls">
         <Select types={types} handleTypeChange={handleTypeChange} />
-        <Query inputValue={query} changeHandler={setQuery} handleFormSubmit={handleFormSubmit} />
+        <Query setQuerySubmitValue={setQuerySubmitValue} />
       </div>
       <PokemonList pokemon={pokemon} loading={loading}></PokemonList>
     </main>
